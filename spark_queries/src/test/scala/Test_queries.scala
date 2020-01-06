28cca3c56dd3b014f2ea100be124f55de4aef788
nidhiarora6262 <48307886+nidhiarora6262@users.noreply.github.com>
2020-01-06 23:33:47 +0530

    Update Test_queries.scala

--- spark_queries/src/test/scala/Test_queries.scala
+++ spark_queries/src/test/scala/Test_queries.scala
@@ -14,33 +14,34 @@ class Test_queries extends FunSuite {
     spark = new SparkSession.Builder().appName("Spark Job for Loading Data").master("local[*]").getOrCreate()
   }
 
-  test("testing RDD") {
+  /*test("testing RDD") {
     val data1 = spark.read.option("header", "true").csv("/home/nidhi/Desktop/nidhiarora/spark_queries/src/main/resources/FL_insurance_sample.csv")
     assert(data1.count() != 0)
-  }
+  }*/
 
 
 
 
 //  test("casting") {
 //
-//    val abc = spark_queries.schema1(data1,"50","123","12","34","34","67","78","12","11","56")
+//    val abc = Spark_Class.schema1("498960", "498960", "49896","498960",  "498960",  "792148.9", "0,9979","0","7890","0")
 //    assert ( data.schema === abc.schema)
 //  }
 
   test("groupBY") {
 
-    val ans = Spark_Class.groupby(,"1","3","4")
+    val ans = Spark_Class.groupby("100735","[-82.461929]","[29.048983]")
     assert(ans.count > 1)
 
   }
 
 
-  /*test("unpivot") {
-    val ans1 = spark_queries.unpivot(data1,"2", "3","34","45","67","67",4,5,8,7,9,4,7,8,7,9)
+  test("unpivot") {
+    val ans1 = spark_queries.unpivot(
+    val query4 = Spark_Class.unpivot("119736" , "FL","CLAY COUNTY" , "Masonry" ," 30.102261","-81.711777",498960, 498960, 49896,498960,498960,  792148.9, 0,9979.2,0,0)
     assert(ans1.collect() === 5)
 
-  }*/
+  }
 }
 
 
